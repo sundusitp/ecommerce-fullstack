8b6ea9df7dfd486689f0079ab5a1ea78b517ecd2
scodev <sundusitpumpeda@sundusitpumpedas-MacBook-Air.local>
2026-01-03 17:07:34 +0700

    fix: update code and database

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = 'https://ecommerce-api-wo04.onrender.com';
+const API_URL = 'http://localhost:3000';
 
 interface Product {
   id: number;
